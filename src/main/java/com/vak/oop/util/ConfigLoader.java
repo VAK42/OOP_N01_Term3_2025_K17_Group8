4e6ae5ea5be89a64ccbaa62e0d9cb7357548b20c
Tran Ha Quang <thaquang2005@gmail.com>
2025-05-06 22:27:39 +0700

    THQ

---
+++ src/main/java/com/vak/oop/util/ConfigLoader.java
@@ -0,0 +1,26 @@
+package com.vak.oop.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static com.vak.oop.view.HomeView.logger;
+
+public class ConfigLoader {
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream input = ConfigLoader.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new IOException("config.properties Not Found!");
+            }
+            properties.load(input);
+        } catch (IOException e) {
+            logger.error("", e);
+        }
+    }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
+}
