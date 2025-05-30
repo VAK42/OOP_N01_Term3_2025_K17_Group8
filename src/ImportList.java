bb83d5731de430ee24f98f441a3d54f6eca75abb
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-05-30 15:37:42 +0700

    !!!

--- src/ImportList.java
+++ src/ImportList.java
@@ -4,31 +4,31 @@ import java.util.List;
 import java.util.UUID;
 
 public class ImportList {
-    private List<Import> imports = new ArrayList<>();
+  private final List<Import> imports = new ArrayList<>();
 
-    public void add(Import imp) {
-        imports.add(imp);
-    }
+  public void add(Import imp) {
+    imports.add(imp);
+  }
 
-    public boolean edit(UUID ipid, Product newProduct, User newUser, Double newPrice, Integer newQuantity, LocalDateTime newDate) {
-        for (Import i : imports) {
-            if (i.getIpid().equals(ipid)) {
-                i.setProduct(newProduct);
-                i.setUser(newUser);
-                i.setPdprice(newPrice);
-                i.setPdquantity(newQuantity);
-                i.setDate(newDate);
-                return true;
-            }
-        }
-        return false;
+  public boolean edit(UUID ipid, Product newProduct, User newUser, Double newPrice, Integer newQuantity, LocalDateTime newDate) {
+    for (Import i : imports) {
+      if (i.getIpid().equals(ipid)) {
+        i.setProduct(newProduct);
+        i.setUser(newUser);
+        i.setPdprice(newPrice);
+        i.setPdquantity(newQuantity);
+        i.setDate(newDate);
+        return true;
+      }
     }
+    return false;
+  }
 
-    public boolean delete(UUID ipid) {
-        return imports.removeIf(i -> i.getIpid().equals(ipid));
-    }
+  public boolean delete(UUID ipid) {
+    return imports.removeIf(i -> i.getIpid().equals(ipid));
+  }
 
-    public List<Import> getAll() {
-        return new ArrayList<>(imports);
-    }
+  public List<Import> getAll() {
+    return new ArrayList<>(imports);
+  }
 }
--- src/UserList.java
+++ src/UserList.java
@@ -3,30 +3,30 @@ import java.util.List;
 import java.util.UUID;
 
 public class UserList {
-    private List<User> users = new ArrayList<>();
+  private final List<User> users = new ArrayList<>();
 
-    public void add(User user) {
-        users.add(user);
-    }
+  public void add(User user) {
+    users.add(user);
+  }
 
-    public boolean edit(UUID userId, String newUsername, String newPassword, String newEmail, String newRole) {
-        for (User u : users) {
-            if (u.getUserId().equals(userId)) {
-                u.setUsername(newUsername);
-                u.setPassword(newPassword);
-                u.setEmail(newEmail);
-                u.setRole(newRole);
-                return true;
-            }
-        }
-        return false;
+  public boolean edit(UUID userId, String newUsername, String newPassword, String newEmail, String newRole) {
+    for (User u : users) {
+      if (u.getUserId().equals(userId)) {
+        u.setUsername(newUsername);
+        u.setPassword(newPassword);
+        u.setEmail(newEmail);
+        u.setRole(newRole);
+        return true;
+      }
     }
+    return false;
+  }
 
-    public boolean delete(UUID userId) {
-        return users.removeIf(u -> u.getUserId().equals(userId));
-    }
+  public boolean delete(UUID userId) {
+    return users.removeIf(u -> u.getUserId().equals(userId));
+  }
 
-    public List<User> getAll() {
-        return new ArrayList<>(users);
-    }
+  public List<User> getAll() {
+    return new ArrayList<>(users);
+  }
 }
