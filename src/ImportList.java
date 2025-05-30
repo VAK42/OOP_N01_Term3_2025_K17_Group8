297c72c0da4057f082f33e7cffd190e3b7f59738
mcongie <phamthiminhngocvbhp@gmail.com>
2025-05-30 15:26:32 +0700

    PTMN

---
+++ src/ImportList.java
@@ -0,0 +1,34 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class ImportList {
+    private List<Import> imports = new ArrayList<>();
+
+    public void add(Import imp) {
+        imports.add(imp);
+    }
+
+    public boolean edit(UUID ipid, Product newProduct, User newUser, Double newPrice, Integer newQuantity, LocalDateTime newDate) {
+        for (Import i : imports) {
+            if (i.getIpid().equals(ipid)) {
+                i.setProduct(newProduct);
+                i.setUser(newUser);
+                i.setPdprice(newPrice);
+                i.setPdquantity(newQuantity);
+                i.setDate(newDate);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean delete(UUID ipid) {
+        return imports.removeIf(i -> i.getIpid().equals(ipid));
+    }
+
+    public List<Import> getAll() {
+        return new ArrayList<>(imports);
+    }
+}
---
+++ src/UserList.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class UserList {
+    private List<User> users = new ArrayList<>();
+
+    public void add(User user) {
+        users.add(user);
+    }
+
+    public boolean edit(UUID userId, String newUsername, String newPassword, String newEmail, String newRole) {
+        for (User u : users) {
+            if (u.getUserId().equals(userId)) {
+                u.setUsername(newUsername);
+                u.setPassword(newPassword);
+                u.setEmail(newEmail);
+                u.setRole(newRole);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean delete(UUID userId) {
+        return users.removeIf(u -> u.getUserId().equals(userId));
+    }
+
+    public List<User> getAll() {
+        return new ArrayList<>(users);
+    }
+}
