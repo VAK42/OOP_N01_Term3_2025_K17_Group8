7c1093bdfeef5517568af16daeee5ad369586264
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-27 15:29:34 +0700

    .

--- test/java/com/vak/oop/model/UserTest.java
+++ test/java/com/vak/oop/model/UserTest.java
@@ -1,1 +1,29 @@
+package com.vak.oop.model;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+  @Test
+  public void testUserFields() {
+    User user = new User();
+    UUID id = UUID.randomUUID();
+    String username = "VAK";
+    String password = "42";
+    String email = "vak@example.com";
+    String role = "admin";
+    user.setUserId(id);
+    user.setUsername(username);
+    user.setPassword(password);
+    user.setEmail(email);
+    user.setRole(role);
+    assertEquals(id, user.getUserId());
+    assertEquals(username, user.getUsername());
+    assertEquals(password, user.getPassword());
+    assertEquals(email, user.getEmail());
+    assertEquals(role, user.getRole());
+  }
+}
